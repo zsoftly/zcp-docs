5baf7f836b2cc38e41191fceb9b208263f978abf
ditahkk <ditah.k@zsoftly.com>
2026-08-28 13:36:39 -0400

    feat: update authentication links to use constants for cloud URLs

--- src/data/site.ts
+++ src/data/site.ts
@@ -5,5 +5,12 @@ export const websiteUrl = (import.meta.env.PUBLIC_WEBSITE_URL ?? 'https://zcp.zs
 
 export const COMMUNITY_PAGE_URL = `${websiteUrl}/community`;
 
+export const cloudUrl = (
+  import.meta.env.PUBLIC_CLOUD_URL ?? 'https://cloud.zcp.zsoftly.ca'
+).replace(/\/+$/, '');
+
+export const CLOUD_LOGIN_URL = `${cloudUrl}/login`;
+export const CLOUD_REGISTER_URL = `${cloudUrl}/register`;
+
 export const COMMUNITY_SLACK_URL =
   'https://join.slack.com/t/zsoftlycloudp-exg5502/shared_invite/zt-469b56b2b-lG5HB2WnuntgwFG4UwfMew';
